52e0b1c12b7bea43c409bea8772b6a4a4131444b
ambernina <rob.ambernina@gmail.com>
2020-06-22 20:07:44 -0600

    src/components/Sidebar.js

--- bangtan-vlives/src/components/Sidebar.js
+++ bangtan-vlives/src/components/Sidebar.js
@@ -1,31 +1,60 @@
 import React from "react";
 import Checkbox from "./Checkbox";
-// import {
-// 	CheckboxFilter,
-// 	TermQuery,
-// 	BoolMust,
-// 	RangeQuery
-// } from "searchkit";
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 const Sidebar = () => {
-	
-
 	// const filter = () => {
-		// if (cardText.includes('RM')) {
-		// 	document.querySelectorAll('card-text').parentNode.style("display", "show");
-		// }
+	// if (cardText.includes('RM')) {
+	// 	document.querySelectorAll('card-text').parentNode.style("display", "show");
 	// }
-
+	// }
+	
 	return (
 		<form>
-			<div className="mb-3" style={{ padding: "10px"}}>
-        <h4>Filters | Search</h4>
-        <br/>
-        <h6>Search by Title</h6>
-        <input className="form-control" type="text" placeholder="Default input" aria-label="default input example"/>
-        <br/>
+			<div className="mb-3" style={{ padding: "10px" }}>
+				<h4>Filters | Search</h4>
+				<br />
+		{/* <h6>Sort by...</h6> */}
+				<div className="dropdown">
+					<button
+						className="btn btn-info dropdown-toggle"
+						type="button"
+						id="dropdownMenuButton"
+						data-toggle="dropdown"
+						aria-expanded="false"
+						// style={{backgroundColor: "#ffffff", color: "black"}}
+					>
+						Sort by...
+					</button>
+					<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<li>
+							<a className="dropdown-item" href="/">
+								Action
+							</a>
+						</li>
+						<li>
+							<a className="dropdown-item" href="/">
+								Another action
+							</a>
+						</li>
+						<li>
+							<a className="dropdown-item" href="/">
+								Something else here
+							</a>
+						</li>
+					</ul>
+				</div>
+				<br />
+				<h6>Search by Title</h6>
+				<input
+					className="form-control"
+					type="text"
+					placeholder="Default input"
+					aria-label="default input example"
+				/>
+				<br />
 				{/* <CheckboxFilter id="RM" title="Members" label="RM" filter={TermQuery("card-text", 'RM')} /> */}
-        <h6>Members</h6>
+				<h6>Members</h6>
 				<Checkbox label="RM" />
 				<Checkbox label="Jin" />
 				<Checkbox label="Suga" />
@@ -33,8 +62,8 @@ const Sidebar = () => {
 				<Checkbox label="Jimin" />
 				<Checkbox label="V" />
 				<Checkbox label="Jungkook" />
-        <br/>
-        <h6>Year</h6>
+				<br />
+				<h6>Year</h6>
 				<Checkbox label="2015" />
 				<Checkbox label="2016" />
 				<Checkbox label="2017" />
