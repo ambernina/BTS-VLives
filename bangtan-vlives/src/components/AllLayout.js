132684bc0d8ef1071280322572ddfa4e3c5421d0
ambernina <rob.ambernina@gmail.com>
2020-06-24 19:03:42 -0600

    removed comments and changed header to maincontent for searching purposes

--- bangtan-vlives/src/components/AllLayout.js
+++ bangtan-vlives/src/components/AllLayout.js
@@ -1,23 +1,12 @@
 import React from "react";
-import Header from "./Header";
-// import MainContent from "./MainContent";
+import MainContent from "./MainContent";
 // import Sidebar from "./Sidebar";
 require("../_variables.scss");
 
 const AllLayout = () => {
 	return (
 		<>
-    <Header />
-			{/* <div className="container-fluid px-4">
-				<div className="row align-items-start">
-					<div className="col-sm-3 col-md-4 col-lg-2 border">
-            <Sidebar />
-					</div>
-					<div className="col-sm-9 col-md-8 col-lg-10">
-						<MainContent />
-					</div>
-				</div>
-			</div> */}
+    <MainContent />
 		</>
 	);
 };
